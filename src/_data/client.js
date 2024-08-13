a7f8c647948f0d7b9577b89c67892b216486db75
Andrew W <awelch45@gmail.com>
2024-08-13 05:19:24 -0500

    modified:   src/_data/client.js 	modified:   src/assets/images/aav-diagram-m.png 	modified:   src/assets/images/aav-diagram.png 	deleted:    src/assets/images/cabinets-m.jpg 	deleted:    src/assets/images/cabinets2-m.jpg 	deleted:    src/assets/images/cabinets2-m.webp 	deleted:    src/assets/images/cabinets2.jpg 	deleted:    src/assets/images/construction-m.jpg 	deleted:    src/assets/images/construction-m.webp 	deleted:    src/assets/images/construction.jpg 	deleted:    src/assets/images/logo-small.png 	deleted:    src/assets/images/skyscraper.jpg 	modified:   src/index.html

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,16 +4,16 @@ module.exports = {
     "phoneForTel": "817-676-6447",
     "phoneFormatted": "(817) 676-6447",
     "address": {
-        "lineOne": "10 Pine Ave",
+        "lineOne": "",
         "city": "Burlington",
         "state": "MA",
         "zip": "01803",
-        "mapLink": "https://www.google.com/maps/search/?api=1&query=10+Pine+Avenue%2C+Burlington%2C+MA+01803&query_place_id=ChIJ3Xa1q0N144kR7tyJiTMRiu8"
+        "mapLink": ""
     },
     "socials": {
         "linkedin": "https://www.linkedin.com/company/aav-gtx-consultation",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     //! https://www.google.com/maps/search/?api=1&query=10+Pine+Avenue%2C+Burlington%2C+MA+01803&query_place_id=ChIJ3Xa1q0N144kR7tyJiTMRiu8
-    "domain": "https://www.example.com"
+    "domain": "https://AAVGTxConsultation.com"
 };
