6e29e55f6b044e62e4afbc461cae4fcd26149c46
Andrew W <awelch45@gmail.com>
2024-08-17 05:16:26 -0500

    guh

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -4,8 +4,9 @@
 
 // helper functions to toggle dark mode
 function enableDarkMode() {document.body.classList.add('dark-mode');localStorage.setItem('theme', 'dark');}
+//comment
 function disableDarkMode() {document.body.classList.remove('dark-mode');localStorage.setItem('theme', 'light');}
-
+//comment
 function detectColorScheme() {let theme = 'light';if (localStorage.getItem('theme')) {theme = localStorage.getItem('theme');} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {theme = 'dark';}theme === 'dark' ? enableDarkMode() : disableDarkMode();}
 // run on page load
 detectColorScheme();
